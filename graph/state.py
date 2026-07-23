98f1da9043afdfdc88aeee0b4f222ab9e5d2498e
Abhinav Pal <meabhi1222@gmail.com>
2026-07-23 15:29:33 +0530

    api updated

--- graph/state.py
+++ graph/state.py
@@ -3,18 +3,6 @@
 # import operator
 
 
-# class AgentState(TypedDict):
-#     query: str
-#     research_output: str
-#     financial_output: str
-#     benchmarking_output: str
-#     trends_output: str
-#     synthesis_output: str
-#     messages: Annotated[List[BaseMessage], operator.add]  #it holds chat messages and operator.add append the message
-#     completed_agents: List[str]
-#     next_agents: List[str] 
-
-
 from typing import TypedDict, List, Annotated
 from langchain_core.messages import BaseMessage
 import operator
@@ -35,4 +23,4 @@ class AgentState(TypedDict):
 
 #     so when 4 agents tried to write query simultaneously LangGraph didn't know which value to keep.
 # take_last just says "if multiple agents write to this field, keep the last one"
-#  — which is the correct behavior for string output fields.
+#   which is the correct behavior for string output fields.
